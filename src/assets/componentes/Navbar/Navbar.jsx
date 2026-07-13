ca77f584a5c0b7a119a98cd7f3860a6c483fa12e
Tyncho <tyncho.zabala@gmail.com>
2026-07-13 00:17:37 -0300

    correcion estuilos header y navbar

--- src/assets/componentes/Navbar/Navbar.jsx
+++ src/assets/componentes/Navbar/Navbar.jsx
@@ -1,26 +1,24 @@
 // Componente Navbar contiene las paginas de navegación del sitio
 // Usa <Link> de react-router-dom para navegación sin recargas
 
+// Componente Navbar actualizado
 import { Link } from "react-router-dom";
 import Styles from "./Navbar.module.css";
 import { useCart } from "../../../Context/CartContext";
 import { useAuth } from "../../../Context/AuthContext";
 
 function Navbar() {
-  // Usamos el hook personalizado para acceder a la función.
   const { getCartQuantity } = useCart();
-  const { user, logout } = useAuth(); //agregamos los datos de autenticación
+  const { user, logout } = useAuth();
 
   const totalItems = getCartQuantity();
 
   return (
     <nav className={Styles.Navbar}>
-      <ul className={Styles.ul}>
+      {/* GRUPO 1: Secciones para usuarios comunes */}
+      <ul className={Styles.ul_comun}>
         <li>
           <Link to="/">Inicio</Link>
-        </li>
-        <li>
-          <Link to="/nosotros">Nosotros</Link>
         </li>
         <li>
           <Link to="/productos">Productos</Link>
@@ -28,28 +26,37 @@ function Navbar() {
         <li>
           <Link to="/carrito">Carrito 🛒 {totalItems} </Link>
         </li>
-        {user ? (
-          <>
-            {(user.rol === "admin" ||
-              user.email?.toLowerCase() === "admin@gmail.com") && (
-              <>
-                <li>
-                  <Link to="/cupones">Gestión de Cupones</Link>
-                </li>
-                <li>
-                  <Link to="/gestion">Gestión de Productos</Link>
-                </li>
-              </>
-            )}
-            <span>¡Hola, {user.email}!</span>
-            <button onClick={logout}>Cerrar Sesión</button>
-          </>
-        ) : (
+        {!user && (
           <li>
             <Link to="/login">Login</Link>
           </li>
         )}
       </ul>
+
+      {/* GRUPO 2: Bloque exclusivo de Admin */}
+      {user && (
+        <ul className={Styles.ul_admin}>
+          {(user.rol === "admin" ||
+            user.email?.toLowerCase() === "admin@gmail.com") && (
+            <>
+              <li>
+                <Link to="/cupones">Gestión de Cupones</Link>
+              </li>
+              <li>
+                <Link to="/gestion">Gestión de Productos</Link>
+              </li>
+            </>
+          )}
+          <li className={Styles.user_saludo}>
+            <span>¡Hola, {user.email}!</span>
+          </li>
+          <li>
+            <button className={Styles.logout_btn} onClick={logout}>
+              Cerrar Sesión
+            </button>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
